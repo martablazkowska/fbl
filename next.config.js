7c125c33b5080d0866a426d8cf21f8716c6c7863
mbl-dev <mablazkowska@gmail.com>
2023-04-24 19:25:08 +0200

    Adds PWA

--- next.config.js
+++ next.config.js
@@ -1,5 +1,26 @@
-module.exports = {
+// const withPWA = require("next-pwa");
+
+// module.exports = withPWA({
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//   },
+//   compiler: {
+//     styledComponents: true,
+//   },
+// });
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable:process.env.NODE_ENV === 'development'
+})
+
+module.exports = withPWA({
   compiler: {
     styledComponents: true,
   },
-};
+  reactStrictMode: true,
+})
+
+
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,7 +25,62 @@ export default class MyDocument extends Document {
     const { styleTags } = this.props as any;
     return (
       <Html>
-        <Head>{styleTags}</Head>
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="assets/apple-touch-icon.png"></link>
+          <link
+              href="assets/iphone5_splash.png"
+              media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
+              rel="apple-touch-startup-image"
+          />
+          <link
+              href="assets/iphone6_splash.png"
+              media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+              rel="apple-touch-startup-image"
+          />
+          <link
+              href="assets/iphoneplus_splash.png"
+              media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
+              rel="apple-touch-startup-image"
+          />
+          <link
+              href="assets/iphonex_splash.png"
+              media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+              rel="apple-touch-startup-image"
+          />
+          <link
+              href="assets/iphonexr_splash.png"
+              media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+              rel="apple-touch-startup-image"
+          />
+          <link
+              href="assets/iphonexsmax_splash.png"
+              media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+              rel="apple-touch-startup-image"
+          />
+          <link
+              href="assets/ipad_splash.png"
+              media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+              rel="apple-touch-startup-image"
+          />
+          <link
+              href="assets/ipadpro1_splash.png"
+              media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+              rel="apple-touch-startup-image"
+          />
+          <link
+              href="assets/ipadpro3_splash.png"
+              media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
+              rel="apple-touch-startup-image"
+          />
+          <link
+              href="assets/ipadpro2_splash.png"
+              media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+              rel="apple-touch-startup-image"
+          />
+          <meta name="theme-color" content="#ffffff" />
+          {styleTags}
+        </Head>
         <body>
           <Main />
           <NextScript />
