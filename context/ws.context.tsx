a9ec17bf75cb66df796c47ced9d39cbee9f4a898
mbl-dev <mablazkowska@gmail.com>
2023-05-19 18:40:24 +0200

    Adds wss

--- context/ws.context.tsx
+++ context/ws.context.tsx
@@ -42,7 +42,7 @@ const WsProvider = ({ children }: WsProviderProps) => {
 
     return () => {
       console.log('return');
-      // socket.close();
+      socket.close();
     };
   }, [WS_URL]);
 
